ba7c30f11b361cffa7109cae870588899bbfc381
John Connolly <jcconnol4@gmail.com>
2022-12-01 21:19:48 -0600

    add day 1 and setup for day 2

--- day-2/index.py
+++ day-2/index.py
@@ -7,20 +7,8 @@ def run_first_problem():
     print("first problem")
     file_lines = file_to_array("day-1/day-1.txt")
     
-    highest_number = 0
-    running_add = 0
-    total_array = []
-    
     for line in file_lines:
-        if line == '':
-            total_array.append(running_add)
-            running_add = 0
-        else:
-            running_add += int(line)
-    
-    
-    print(sorted(total_array))
-            
+        print(line)            
     
     
     
