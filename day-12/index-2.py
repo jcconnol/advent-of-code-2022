3b224a5e9d3b03d53099969afa5b365067ea1d41
John Connolly <jcconnol4@gmail.com>
2022-12-11 09:11:26 -0600

    add template for day 12

---
+++ day-12/index-2.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import numpy as np
+import array
+from collections import defaultdict
+
+
+def run_first_problem():        
+    file_lines = file_to_array("input.txt")
+
+    for line in file_lines:
+        print(line)
+
+    
+    
+        
+    
+    
+    
+    
+    
+def isUniqueChars(st):
+ 
+    # String length cannot be more than
+    # 256.
+    if len(st) > 256:
+        return False
+ 
+    # Initialize occurrences of all characters
+    char_set = [False] * 128
+ 
+    # For every character, check if it exists
+    # in char_set
+    for i in range(0, len(st)):
+ 
+        # Find ASCII value and check if it
+        # exists in set.
+        val = ord(st[i])
+        if char_set[val]:
+            return False
+ 
+        char_set[val] = True
+ 
+    return True
+    
+def file_to_array(file_name):
+    line_array = []
+    
+    full_file_name = os.path.join(os.path.dirname(__file__), file_name)
+    
+    with open(full_file_name) as my_file:
+        for line in my_file:
+            line_array.append(line.strip())
+            
+    return line_array
+
+if __name__ == '__main__':
+    run_first_problem()
